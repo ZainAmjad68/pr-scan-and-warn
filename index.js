82983e60f183831d264e3638ec8a2b166dbbcf15
ZainAmjad68 <zainamjad68@yahoo.com>
2023-07-07 17:28:05 +0500

    slight change

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const keyWords = core.getInput('words-to-scan-for');
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-  core.setOutput("annotations", "echo '::error file=app.js,line=1::Missing semicolon'");
+  core.setOutput("annotations", "echo '::error file=src/main.tsx,line=1::Missing semicolon'");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
