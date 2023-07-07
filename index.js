a077843c8988129d3d251d3eded218e43125540b
ZainAmjad68 <zainamjad68@yahoo.com>
2023-07-07 17:40:46 +0500

    fix index

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const github = require('@actions/github');
 try {
   // `words-to-scan-for` input defined in action metadata file
   const keyWords = core.getInput('words-to-scan-for');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("annotations", "echo '::error file=src/main.tsx,line=1::Missing semicolon'");
+  core.setOutput("annotations", `echo '::error file=src/main.tsx,line=1::You Should Not be using ${keyWords} here.'`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
