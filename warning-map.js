a14346758e7ebe362ad834273deac7fbc26a31a7
ZainAmjad68 <zainamjad68@yahoo.com>
2023-07-14 10:43:08 +0500

    implement logic to find matches and create metadata for annotations

---
+++ warning-map.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'destroy()' : 'Be Careful About Using Destroy. It can have unintentional consequences.',
+    'dropTable' : 'Using DropTable is not recommended. It can cause irrevocable data loss.'
+};
