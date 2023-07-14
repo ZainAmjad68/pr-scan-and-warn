57b9841554e4c5b93e66928a008248c782c2b019
ZainAmjad68 <zainamjad68@yahoo.com>
2023-07-14 10:43:32 +0500

    remove unnecessary experiment code

--- index.js
+++ index.js
@@ -2,70 +2,27 @@ const exec2 = require('@actions/exec');
 const core = require('@actions/core');
 const github = require('@actions/github');
 const {Toolkit} = require('actions-toolkit');
-const {exec, execSync} = require('child_process');
 const getDiffWithLineNumbers = require('./git_diff');
+const extractMatchingLines = require('./analyze');
+
 
 Toolkit.run(async (tools) => {
   try {
     // `words-to-scan-for` input defined in action metadata file
     const githubToken = core.getInput('token');
-    const octokit = github.getOctokit(githubToken);
-
-    const tools = new Toolkit({
-      token: githubToken,
-    });
-
-    const { data: pullRequest } = await octokit.rest.pulls.get({
-        owner: tools.context.repo.owner,
-        repo: tools.context.repo.repo,
-        pull_number: tools.context.payload.pull_request.number,
-        mediaType: {
-          format: 'diff'
-        }
-    });
-
-    const resp = await octokit.rest.pulls.listFiles({
-      owner: tools.context.repo.owner,
-      repo: tools.context.repo.repo,
-      pull_number: tools.context.payload.pull_request.number,
-      per_page: 100
-    });
-
-    
-
-    console.log('Pull Request Diff: ', pullRequest);
-    console.log('Pull Request ListFiles: ', resp.data);
-    const diff = core.getInput('diff');
-    console.log('Diff from git-diff-action: ', diff);
-
 
     const keyWords = core.getInput('words-to-scan-for');
-    core.setOutput("annotations", `echo '::error file=src/main.tsx,line=1::You Should Not be using ${keyWords} here.'`);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-
-    console.log(`The event payload: ${payload}`);
-    console.log('context PR base ref: ', github.context.payload.pull_request.base.ref);
-
-    let execChangedFiles = await exec2.exec(`git diff --name-only HEAD^1`);
-    let getExecOutputChangedFiles = await exec2.getExecOutput(`git diff --name-only HEAD^1`);
-    console.log('execChangedFiles: ', execChangedFiles);
-    console.log('getExecOutputChangedFiles: ', getExecOutputChangedFiles);
-
-
-    let changedFiles = await execSync(`git diff --name-only HEAD^1`).toString();
-    let files = changedFiles.split('\n');
-    files.pop();
-    console.log('files changed: ', files);
   
     let prData = await getDiffWithLineNumbers('HEAD^1');
-    console.log('All the Changes: ', prData);
-    for (const obj of prData) {
-      console.log('-----------------------');
-      console.log('fileName:', obj.fileName);
-      console.log('data:', obj.data);
-      console.log('-----------------------');
-    }
+    let filesWithMatches = await extractMatchingLines(prData, keyWords);
+
+    // now use this data to first form annotations for Warnings
+    // display those Annotations
+
+    // work on incorporating test coverage stuff as well
+    // parse how the coverage file is structured then
+    // use that to form annotations and
+    // display those annotations as well
 
   } catch (error) {
     tools.exit.failure(error.message);
