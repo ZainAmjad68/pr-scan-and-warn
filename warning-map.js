fe10aa0ec52512cdb2a4cb6ba9910ba40079e3c8
ZainAmjad68 <zainamjad68@yahoo.com>
2023-09-14 11:38:56 +0500

    add `Promise.all` and `Promise.map` to warning map

--- warning-map.js
+++ warning-map.js
@@ -1,4 +1,6 @@
 module.exports = {
     'destroy()' : 'Be Careful About Using Destroy. It can have unintentional consequences.',
-    'dropTable' : 'Using DropTable is not recommended. It can cause irrevocable data loss.'
+    'dropTable' : 'Using DropTable is not recommended. It can cause irrevocable data loss.',
+    'Promise.all' : `Be Careful!\n1. This may cause performance issues due to concurrent execution.\n2. If one promise fails, it can potentially block the resolution of other promises.`,
+    'Promise.map' : `Be Careful!\n1. This may cause performance issues due to concurrent execution.\n2. If one promise fails, it can potentially block the resolution of other promises.`
 };
